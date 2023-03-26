9d93744bde0ad9eab7391a251a237441428158db
valentina0612 <vbecag@correo.usbcali.edu.co>
2023-03-26 16:33:03 -0500

    Proyecto Saludablemente Delicioso

---
+++ proyectorestaurante/saludablementedelicioso/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SaludablementedeliciosoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'saludablementedelicioso'
